93267666da4bb5b3b6db9e179a507520c7bebda0
AfonsoBolinha <afonsomartinspaivamiguel@gmail.com>
2022-05-04 17:35:38 +0100

    Timer Funcional(BArcoTier1 BarcoTier2)

--- src/main/java/com/example/batalhanaval/HelloController.java
+++ src/main/java/com/example/batalhanaval/HelloController.java
@@ -1,14 +1,45 @@
 package com.example.batalhanaval;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+import javafx.util.Duration;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+public class HelloController implements Initializable {
+    Timeline timeline;
+    LocalTime time=LocalTime.parse("00:00");
+    DateTimeFormatter dtf=DateTimeFormatter.ofPattern("mm:ss");
+
+    public Label timer;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        timeline = new Timeline(new KeyFrame(Duration.millis(1000), ae -> incrementTime()));
+        timeline.setCycleCount(Animation.INDEFINITE);
     }
+
+    private void incrementTime() {
+        time = time.plusSeconds(1);
+        timer.setText(time.format(dtf));
+    }
+    @FXML
+    private void startTimer(ActionEvent event) {
+        timeline.play();
+    }
+    @FXML
+    private void pauseTimer(ActionEvent event){
+        timeline.pause();
+    }
+
+
 }
